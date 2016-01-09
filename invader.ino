1cc179c20854852372c606887e2cdcd1c5112a3d
Miguel Dorta <dortamiguel@gmail.com>
2016-01-09 14:45:17 +0000

    added new write to screen system

--- invader.ino
+++ invader.ino
@@ -9,7 +9,7 @@ void setup() {
 	lc.setIntensity(0,5);  // Set intensity levels
 	lc.clearDisplay(0);  // Clear Display
 
-	// Player vars
+	// player vars
 	int pos_x = 0;
 	int pos_y = 0;
 }
@@ -17,16 +17,29 @@ void setup() {
 char lineOneC[8] = {1,1,1,0,1,1,1,1};
 char lineOneB = 0;
 
+char screen[8][8] = {
+	{1,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0}
+};
+
 byte charToByte(char lineC[8]) {
+	// lineC stands for line of chars and lineB for a byte line
 	char lineB = 0;
 	for (int i = 0; i < 8; i++) { lineB |= lineC[i] << (7-i); }
 	return lineB;
 }
 
+void movePlayer() {
+	
+}
+
 void loop() {
-	for (int i = 0; i < 8; i++)
-	{
-		lc.setRow(0,i,charToByte(lineOneC));
-	}
+	for (int i = 0; i < 8; i++) { lc.setRow(0,i,charToByte(screen[i])); } // draws the screen var to the lcd display
 	delay(delayTime);
 }
